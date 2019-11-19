9d923c16cf372330a75f7373c476e228c8fbfe38
vempari <Vempari_2@mail.ru>
2019-11-19 21:52:19 +0300

    Finish

--- src/main/java/ru/kozhukhovskii/lessons/Lesson1.java
+++ src/main/java/ru/kozhukhovskii/lessons/Lesson1.java
@@ -3,7 +3,7 @@ package ru.kozhukhovskii.lessons;
 import java.io.File;
 
 public class Lesson1 implements Lesson {
-    File file = new File("/src/main/resources/Book 1. Java.JDK, InteliJ IDEA, Git.pdf");
+    File file = new File("/src/main/resources/Book 1. Java, JDK, IDEA, GIT.pdf");
 
     @Override
     public File getPresentation() {
